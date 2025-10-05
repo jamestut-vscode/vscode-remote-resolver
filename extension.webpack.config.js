689ad5ee4db37de5af48c2a9912f315b71f323b6
James <aslan.jnn@gmail.com>
2025-10-05 21:51:34 +1100

    Update CJS to JS module for webpack config

--- extension.webpack.config.js
+++ extension.webpack.config.js
@@ -2,19 +2,17 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+// @ts-check
+import withDefaults from '../shared.webpack.config.mjs';
+import path from 'path';
 
-'use strict';
-
-const withDefaults = require('../shared.webpack.config');
-const path = require('path');
-
-module.exports = withDefaults({
-	context: __dirname,
+export default withDefaults({
+	context: import.meta.dirname,
 	entry: {
 		extension: './src/extension.ts',
 	},
 	output: {
 		filename: 'extension.js',
-		path: path.join(__dirname, 'dist')
+		path: path.join(import.meta.dirname, 'dist')
 	}
 });
